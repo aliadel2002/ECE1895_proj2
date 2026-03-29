baf1ea2bc5a1bc805831222440fe7dfd41a7f6b7
Tyler-Woodard <taw137@pitt.edu>
2026-03-29 18:27:25 -0400

    Update Bopit_Main.ino

--- Bopit_Main.ino
+++ Bopit_Main.ino
@@ -1,5 +1,4 @@
 #include <Adafruit_LSM6DS3TRC.h>
-
 #include <Wire.h>
 
 // Pin Constants
@@ -7,27 +6,45 @@ constexpr uint8_t Button = 9;
 constexpr uint8_t LED1 = 8;
 
 // I2C Config
-constexpr uint8_t P1_I2C_ADR = 0x00;
-constexpr uint8_t P2_I2C_ADR = 0x00;
+constexpr uint8_t IMU_1_ADDR = 0x6A;
+//constexpr uint8_t IMU_2_ADDR = 0x6B;
+
+Adafruit_LSM6DS3TRC player1;
+//Adafruit_LSM6DS3TRC player2;
 
 void setup() {
-  // Setup I2C and Serial
-  Wire.begin();
-  Serial.begin(115200);
-  Serial.println("test");
-
   // Set Pin Modes
   pinMode(Button, INPUT_PULLUP);
   pinMode(LED1, OUTPUT);
+
+  // Setup I2C and Serial
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(10);
+  }
+  Serial.println("TEST");
+
+  // Initialize Sensors
+  if (!player1.begin_I2C(IMU_1_ADDR)) {
+    Serial.println("Couldn't find IMU 1 at 0x6A");
+  }
+
+  // if (!player2.begin_I2C(IMU_2_ADDR)) {
+  //   Serial.println("Couldn't find IMU 2 at 0x6B");
+  // }
+
 }
 
 void loop() {
+  sensors_event_t accel1, gyro1, temp1;
+  //sensors_event_t accel2, gyro2, temp2;
+  delay(500);
 
-  while(digitalRead(Button) == 1){
-    digitalWrite(LED1, HIGH);
-    delay(500);
-    digitalWrite(LED1, LOW);
-    delay(500);
-  }
+  // while(digitalRead(Button) == 1){
+  //   digitalWrite(LED1, HIGH);
+  //   delay(500);
+  //   digitalWrite(LED1, LOW);
+  //   delay(500);
+  // }
   
 }
